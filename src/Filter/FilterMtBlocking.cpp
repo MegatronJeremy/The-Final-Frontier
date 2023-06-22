aae636d30aa45ae48eb1f21c9b13f4aae8824acf
Vuk Djordjevic <vukd10@gmail.com>
2023-06-22 12:40:44 +0200

    Added const parameters

--- src/Filter/FilterMtBlocking.cpp
+++ src/Filter/FilterMtBlocking.cpp
@@ -12,9 +12,9 @@ void filter_mt_blocking(Image &src, Image &dst, const double *kernel, int N) {
     uint32_t num_threads = std::thread::hardware_concurrency();
     std::vector<std::thread> threads(num_threads);
 
-    int block_row_size = static_cast<int>((src.height - 1) / num_threads + 1);
+    const int block_row_size = static_cast<int>((src.height - 1) / num_threads + 1);
 
-    int block_size = static_cast<int>(get_cache_line_size(L1_CACHE_ID) / sizeof(uint8_t));
+    const int block_size = static_cast<int>(get_cache_line_size(L1_CACHE_ID) / sizeof(uint8_t));
 
     const int center = N / 2;
     const double MIN = 0;
@@ -22,13 +22,13 @@ void filter_mt_blocking(Image &src, Image &dst, const double *kernel, int N) {
 
     for (int thread_id = 0; thread_id < num_threads; ++thread_id) {
         threads[thread_id] = std::thread([&, thread_id]() {
-            int block_row_start = std::max(thread_id * block_row_size, center); // to start indexing from center
-            int block_row_end = std::min(src.height - center, (thread_id + 1) * block_row_size);
+            const int block_row_start = std::max(thread_id * block_row_size, center); // to start indexing from center
+            const int block_row_end = std::min(src.height - center, (thread_id + 1) * block_row_size);
 
             for (int rowi = block_row_start; rowi < block_row_end; rowi += block_size) {
                 for (int coli = center; coli < src.width - center; coli += block_size) {
-                    int inner_row_end = std::min(block_row_end, rowi + block_size);
-                    int inner_col_end = std::min(src.width - 1, coli + block_size);
+                    const int inner_row_end = std::min(block_row_end, rowi + block_size);
+                    const int inner_col_end = std::min(src.width - 1, coli + block_size);
 
                     for (int row = rowi; row < inner_row_end; row++) {
                         for (int col = coli; col < inner_col_end; col++) {
@@ -38,12 +38,12 @@ void filter_mt_blocking(Image &src, Image &dst, const double *kernel, int N) {
 
                             for (int i = 0; i < N; ++i) {
                                 for (int j = 0; j < N; ++j) {
-                                    size_t image_row = (row + i - center);
-                                    size_t image_column = (col + j - center);
+                                    const size_t image_row = (row + i - center);
+                                    const size_t image_column = (col + j - center);
 
-                                    size_t index = image_row * src.width + image_column;
+                                    const size_t index = image_row * src.width + image_column;
 
-                                    int kernel_index = i * N + j;
+                                    const int kernel_index = i * N + j;
 
                                     r += src.R[index] * kernel[kernel_index];
                                     g += src.G[index] * kernel[kernel_index];
@@ -95,7 +95,7 @@ void sobel_mt_blocking(Image &src, Image &dst) {
                 for (int coli = center; coli < src.width - center; coli += block_size) {
                     int inner_row_end = std::min(block_row_end, rowi + block_size);
                     int inner_col_end = std::min(src.width - 1, coli + block_size);
-                    
+
                     for (int row = rowi; row < inner_row_end; row++) {
                         for (int col = coli; col < inner_col_end; col++) {
                             double gx = 0;
